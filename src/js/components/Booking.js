cfef7a052ff9770c1f4c7a6644df5b5f2f2f8be2
Marek Rumianek <marek.mail7@gmail.com>
2020-10-08 14:26:38 +0200

    added data pick

--- src/js/components/Booking.js
+++ src/js/components/Booking.js
@@ -1,6 +1,7 @@
 import {AmountWidget} from './AmountWidget.js';
 import {templates, select} from '../settings.js';
 import{utils} from '../utils.js';
+import { DataPicker } from './DataPicker.js';
 
 export class Booking{
   constructor(element){
@@ -14,7 +15,8 @@ export class Booking{
     this.dom.wrapper = element;
     this.map = utils.createDOMFromHTML(generatedHTML);
     this.dom.peopleAmount = this.map.querySelector(select.booking.peopleAmount);
-    this.dom.hoursAmount = this.map.querySelector(select.booking.hoursAmount);    
+    this.dom.hoursAmount = this.map.querySelector(select.booking.hoursAmount);
+    this.dom.datePicker = this.map.querySelector(select.widgets.datePicker.wrapper);    
 
     element.appendChild(this.map);
   }
@@ -22,5 +24,6 @@ export class Booking{
   initWidget(){
     this.dom.peopleAmount.widget = new AmountWidget(this.dom.peopleAmount);
     this.dom.hoursAmount.widget = new AmountWidget(this.dom.hoursAmount);
+    this.dataPicker = new DataPicker(this.dom.datePicker);
   }
 }
---
+++ src/js/components/DataPicker.js
@@ -0,0 +1,43 @@
+import { utils } from '../utils.js';
+import { select } from '../settings.js';
+import {BaseWidget} from './BaseWidget.js';
+import flatpickr from'flatpickr';
+
+export class DataPicker extends BaseWidget {
+  constructor(wrapper){
+    super(wrapper, utils.dateToStr(new Date()));
+    this.dom.input = this.dom.wrapper.querySelector(select.widgets.datePicker.input);
+
+    this.initPlugin();
+  }
+  
+  initPlugin(){
+    this.minDate = utils.dateToStr(new Date(this.value));
+    this.maxDate = utils.dateToStr(utils.addDays(this.value, 14));
+    console.log(this.minDate, '', this.maxDate);
+    
+    this.dom.datePicker = flatpickr(this.dom.input, {
+      defaultDate: this.minDate,
+      maxDate: this.maxDate,
+      locale:{
+        firstDayOfWeek: 1,
+      },
+      disable:[
+        function(date){
+          return (date.getDay() === 0);
+        }],
+      onChange: function(selectedDate, dateStr){
+        this.value = dateStr;
+      }
+    });
+  }
+
+  parseValue(){
+  }
+
+  isValid(){
+  }
+
+  renderValue(){
+  }
+}
